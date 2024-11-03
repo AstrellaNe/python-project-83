0082bcef0ffec3b61f51bb273ea986de0ce48e74
Astrella.Ne <mycuttingapp@gmail.com>
2024-11-03 22:03:14 +0300

    fix flash-messages | added validators | upd toml

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import psycopg2
 import os
 import datetime
-from flask import Flask, request, redirect, flash, render_template
+from flask import Flask, request, redirect, flash, render_template, url_for
 from db_connection import add_url, get_all_urls, get_connection
+import validators  # Библиотека для валидации URL
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
 
-# фильтр для форматирования даты и времени без тысячных сек
+# Фильтр для форматирования даты и времени без тысячных сек
 @app.template_filter('datetimeformat')
 def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
     if isinstance(value, datetime.datetime):
@@ -25,13 +26,30 @@ def index():
 @app.route('/add-url', methods=['POST'])
 def add_url_route():
     url = request.form['url']
-    if not url or len(url) > 255:
-        flash('Некорректный URL', 'error')
-        return redirect('/')
 
-    add_url(url)
-    flash('URL успешно добавлен', 'success')
-    return redirect('/')
+    # Проверка на корректность URL
+    if not url or not validators.url(url) or len(url) > 255:
+        flash('Некорректный URL', 'error')
+        return redirect(url_for('index'))
+
+    # Проверка на уникальность URL
+    conn = get_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM urls WHERE name = %s;", (url,))
+                existing_url = cursor.fetchone()
+                if existing_url:
+                    flash('Этот URL уже существует.', 'error')
+                    return redirect(url_for('index'))
+                add_url(url)
+                flash('URL успешно добавлен', 'success')
+        except psycopg2.Error as e:
+            flash(f"Ошибка при добавлении URL: {e}", 'error')
+        finally:
+            conn.close()
+
+    return redirect(url_for('index'))
 
 
 @app.route('/urls/<int:id>')
@@ -46,7 +64,7 @@ def show_url(id):
                     return render_template('url_details.html', url=url)
                 else:
                     flash('URL не найден', 'error')
-                    return redirect('/')
+                    return redirect(url_for('index'))
         except psycopg2.Error as e:
             flash(f"Ошибка при получении данных: {e}", 'error')
         finally:
@@ -66,4 +84,4 @@ def delete_url(id):
             flash(f"Ошибка при удалении URL: {e}", 'error')
         finally:
             conn.close()
-    return redirect('/')
+    return redirect(url_for('index'))
