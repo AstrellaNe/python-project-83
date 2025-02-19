855741036cad7f3fa5aa9328fe6784df61a3cec0
Astrella.Ne <mycuttingapp@gmail.com>
2025-02-19 17:53:21 +0300

    upd build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Установка uv и установка зависимостей
+# Устанавливаем uv и зависимости
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+
+# Устанавливаем зависимости проекта
 make install
+
+# Загружаем SQL-схему в базу
+psql -a -d $DATABASE_URL -f database.sql
